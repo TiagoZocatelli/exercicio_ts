f1396dd7f6afa84ed7ab727ade8e0d072bd0443b
Tiago <zokahtv@gmail.com>
2024-08-05 20:15:00 -0300

    exercicio_ts

---
+++ full.js
@@ -0,0 +1,11 @@
+"use strict";
+function soma(a, b) {
+    const mult = a * b;
+    return mult;
+}
+console.log(soma(10, 50));
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+const seuNome = "Tiago";
+console.log(dizOla(seuNome));
---
+++ full.ts
@@ -0,0 +1,13 @@
+function soma(a:number, b:number): number {
+    const mult = a * b;
+    return mult;
+}
+
+console.log(soma(10,50))
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const seuNome = "Tiago";
+console.log(dizOla(seuNome));
